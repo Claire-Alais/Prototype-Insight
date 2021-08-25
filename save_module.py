06ac3c5bb0dc8e8fdb0f52a87f35de2013a70d9b
Claire-Alais <claire_alais@hotmail.com>
2021-08-25 12:04:37 +0200

    pickle ejected, graph improved

--- save_module.py
+++ save_module.py
@@ -4,31 +4,16 @@ Created on Tue Aug 10 14:29:16 2021
 
 @author: clair
 
-/!\  /!\  /!\
-allows to save networkx graphs but safety problems due to unpickling part
-(unpickling a file that has been tampered with coul provoke running an unkown script)
-I keep pickle for now as it is the simplest, but another solution is to be found.
-networkx.readwrite.json_graph.node_link_data / networkx.readwrite.json_graph.node_link_graph seems promising for graphs
+allows to save networkx graphs in csv files
 """
-
-import pickle
-
-
-def save_obj(obj, name ):
-    with open(name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
-def load_obj(name ):
-    with open(name + '.pkl', 'rb') as f:
-        return pickle.load(f)
 
 import pandas as pd  
 import networkx  
     
 def save_graph(graph, name):
-    Gpanda = networkx.convert_matrix.to_pandas_edgelist(graph, source='source', target='target', edge_key='edgeKey')
+    Gpanda = networkx.convert_matrix.to_pandas_edgelist(graph, edge_key='edgeKey')
     Gpanda.to_csv(name + '.csv')
     
 def load_graph(name):
-    Gpanda = pd.read_csv(name + '.csv')
-    return networkx.convert_matrix.from_pandas_edgelist(Gpanda, source='source', target='target')
+    Gpanda = pd.read_csv(name)
+    return networkx.convert_matrix.from_pandas_edgelist(Gpanda, source='source', target='target', edge_attr=True)
